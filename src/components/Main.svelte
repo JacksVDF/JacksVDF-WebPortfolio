59629a5332a37c8c90b742fcd5b176d5d8ad2a8c
Julian A. Cardona <jacksvdf@gmail.com>
2024-02-21 16:05:20 -0500

    feat: :sparkles: Main Component | Additional section for about me

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -11,6 +11,11 @@
     {name: 'DevSecOps', icon: 'fa-solid fa-microchip'}, 
     {name: 'Linux OS', icon: 'fa-solid fa-terminal'}, 
     ]
+  let benefits = [
+    {name: 'A self taught learner', description: 'I am someone who thrives on self-improvement and takes charge of my own learning journey. Whether it is mastering a new programming language, delving into graphic design, or exploring diverse subjects, I find joy in the process of self-education. My ability to independently acquire and apply knowledge is a testament to my dedication to continuous personal and professional development.'},
+    {name: 'A resilient professional', description: 'In the professional arena, I pride myself on my resilience and adaptability. Challenges do not deter me; they fuel my determination. I have a proven track record of bouncing back from setbacks with a positive mindset, maintaining composure under pressure, and consistently delivering results. My resilience makes me a reliable and steady force in any work environment.'},
+    {name: 'An Cloud and Cybersecurity enthusiast', description: 'I am genuinely passionate about the dynamic realms of cloud computing and cybersecurity. Staying abreast of the latest technological advancements is not just a task for me; it is a fascination. From understanding the intricacies of cloud-based solutions to ensuring the security of digital landscapes, my enthusiasm drives me to continually explore and implement cutting-edge strategies in the ever-evolving field of information technology.'},
+  ]
 
   onMount(()=>{
     lottie.loadAnimation({
@@ -67,8 +72,31 @@
       </Step>
       <Step step={steps[2]}>
         <p>These Linux Open Source projects are meant to be used for the community and my Research Group GEHIRN</p>
-        <strong class="text-violet-400">LinuxOS, Bash, Terminal & OS Software</strong> All these tech is for OpenSource projects.
+        <strong class="text-violet-400">LinuxOS, Bash, Terminal & OS Software</strong> All these tech is for OpenSource.
       </Step>
+    </div>
+  </section>
+
+  <section id="about" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+    <div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4">
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">A bit <span class="poppins text-violet-400">about</span> me.</h3>
+    </div>
+    <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+            0{index+1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
     </div>
   </section>
 
